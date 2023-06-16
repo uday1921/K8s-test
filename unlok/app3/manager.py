3b15a744cad87811014e4fe723fe7004e67c4576
QHKT64 <QHKT64@motorolasolutions.com>
2023-06-16 14:39:18 +0530

    test commit

---
+++ unlok/app3/manager.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, Response, jsonify
+from flask_restful import Api, Resource
+import logging
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+log = app.logger
+log.setLevel(logging.DEBUG)
+
+
+# @app.route("/")
+# def hello_world():
+    # return "Hello, World!"
+
+class Backend(Resource):
+    def __init__(self):
+        logging.info("Into INIT Method...")
+        name = "uday"
+        logging.info("Name:{}".format(name))
+    @staticmethod
+    def post(self):
+        data = json.loads(request.data)
+        logging.info("Received data:{}".format(data))
+    def get(self):
+        return "Hii UDAY From application3"
+
+api.add_resource(Backend, '/', endpoint="/")
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=8080)
